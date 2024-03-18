4e5e633cbef98d9d38858623220280aa6aa75114
Connor Stack <connorstack123@gmail.com>
2024-03-18 14:36:07 -0500

    Not sure
    ...

--- PathoGenocide/Assets/Player.cs
+++ PathoGenocide/Assets/Player.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,6 +7,7 @@ public class Player : MonoBehaviour
 {
     [Header("Stats")]
     [SerializeField] float speed = 5f;
+    [SerializeField] float maxVerticalSpeed = 10;
 
     public enum PlayerMovementType { tf, physics };
     [SerializeField] PlayerMovementType movementType = PlayerMovementType.tf;
@@ -15,6 +17,8 @@ public class Player : MonoBehaviour
 
     [Header("Flavor")]
     [SerializeField] string playerName = "Leuk";
+    [SerializeField] private GameObject body;
+    [SerializeField] List<AnimationStateChanger> animationStateChangers;
     Rigidbody2D rigidBody;
 
     void Start()
@@ -28,14 +32,51 @@ public class Player : MonoBehaviour
         }else if (movementType == PlayerMovementType.physics){
             MovePlayerRigidBody(direction);
         }
+        //set animation
+        if(direction != Vector3.zero){
+            foreach(AnimationStateChanger asc in animationStateChangers){
+                asc.ChangeAnimationState("LeukWalk1_Clip");
+            }
+        }else{
+            foreach(AnimationStateChanger asc in animationStateChangers){
+                asc.ChangeAnimationState("LeukIdle_Clip");
+            }
+        }
     }
 
     public void MovePlayerTransform(Vector3 direction){
         transform.position += direction * Time.deltaTime * speed;
+        if (direction.x < 0){
+            body.transform.localScale = new Vector3(-1, 1, 1); // Flip left
+        } else if (direction.x > 0) {
+            body.transform.localScale = new Vector3(1, 1, 1); // Flip right
+        }
+    }
+
+    public void MovePlayerRigidBody2(Vector3 direction){
+        Vector3 currentVelocity = new Vector3(0, rigidBody.velocity.y, 0);
+        rigidBody.velocity = (currentVelocity) + (direction * speed);
+        if(rigidBody.velocity.x < 0){
+            body.transform.localScale = new Vector3(-1, 1, 1);
+            Debug.Log("Flip!");
+        }
     }
 
     public void MovePlayerRigidBody(Vector3 direction){
-        Vector3 currentVelocity = new Vector3(0, rigidBody.velocity.y, 0);
-        rigidBody.velocity = (currentVelocity) + (direction * speed);
+    // Calculate the desired velocity based on input direction
+    Vector3 desiredVelocity = new Vector3(direction.x * speed, direction.y * speed, 0);
+
+    // Optionally, you can apply a limit to the vertical movement speed to prevent extreme behavior
+    desiredVelocity.y = Mathf.Clamp(desiredVelocity.y, -maxVerticalSpeed, maxVerticalSpeed);
+
+    // Apply the desired velocity to the rigidbody
+    rigidBody.velocity = desiredVelocity;
+
+    // Flip the sprite based on horizontal movement
+    if(direction.x < 0){
+        body.transform.localScale = new Vector3(-1, 1, 1);
+    } else if (direction.x > 0) {
+        body.transform.localScale = new Vector3(1, 1, 1);
     }
+}
 }
