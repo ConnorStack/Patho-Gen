0ff480adebcdb6be97da20cd65514c57825dcbe3
Connor Stack <connorstack123@gmail.com>
2024-04-24 14:35:58 -0500

    Establish a lifetime for DNAProcessingZones
    DNAProcessingZones now last for a set amount of time, and spawn at a set rate

--- PathoGenocide/Assets/DNAZoneController.cs
+++ PathoGenocide/Assets/DNAZoneController.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class DNAZoneController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public GameObject zonePrefab;
+    public float zoneLifetime = 10f;
+    public float spawnInterval = 8f;
+    public Vector2 mapBoundsMin;
+    public Vector2 mapBoundsMax;
+    private void Start()
     {
-        
+        // spawnInterval = 1f;
+        InvokeRepeating("SpawnZone", 0f, spawnInterval);  // Adjust the spawn interval as needed
+    }
+    void SpawnZone()
+    {
+        Vector2 spawnPosition = new Vector2(
+            Random.Range(mapBoundsMin.x, mapBoundsMax.x),
+            Random.Range(mapBoundsMin.y, mapBoundsMax.y)
+        );
+
+        GameObject zone = Instantiate(zonePrefab, spawnPosition, Quaternion.identity);
+        Destroy(zone, zoneLifetime);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
